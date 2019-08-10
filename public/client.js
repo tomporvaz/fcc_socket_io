e9bcad692cc5906c627e0f7ca4860f651ed61228
tomporvaz <tomporvaz@gmail.com>
2019-08-10 18:44:21 -0400

    Added message in client js announcing users.

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   /*global io*/
   const socket = io();
-  const jquery = require('jquery');
+  const jquery = jquery();
   
    
   // Form submittion with new message in field with id 'm'
@@ -16,6 +16,9 @@ $( document ).ready(function() {
   socket.on('user', function(data){
     console.log(data);
     $('#num-users').text(`${data.currentUsers} users online`);
+    $('#messeages')
+    .append($('<li>'))
+    .text(`${data.name} has ${data.connected ? joined : left} the chat`);
   })
 
   
